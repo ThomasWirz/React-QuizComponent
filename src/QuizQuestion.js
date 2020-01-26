59ef5ba817d7d277531ff9f42cdca01ec17edfc0
Thomas Wirz <t.wirz@adeon.ch>
2020-01-26 12:31:02 +0100

    correct html Tag

--- src/QuizQuestion.js
+++ src/QuizQuestion.js
@@ -5,9 +5,11 @@ class QuizQuestion extends Component{
     render(){
         return (
             <main>
-                <selection>
-                    <p>{this.props.quiz_question.instruction_text}</p>
-                </selection>
+                <section>
+                    <p>
+                        {this.props.quiz_question.instruction_text}
+                    </p>
+                </section>
                 <section className="buttons">
                     <ul>
                        <li>{this.props.quiz_question.answer_options[0]}</li>
