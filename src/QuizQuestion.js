de76211c2d504ccd0ee65a6c4ac2f2fb8772f8c0
Thomas Wirz <t.wirz@adeon.ch>
2020-01-26 12:25:24 +0100

    Build QuizQuestion Component

---
+++ src/QuizQuestion.js
@@ -0,0 +1,20 @@
+import React, {Component} from 'react'
+
+class QuizQuestion extends Component{
+
+    render(){
+        return (
+            <main>
+                <selection>
+                    <p>{this.props.quiz_question.instruction_text}</p>
+                </selection>
+                <section className="buttons">
+                    <ul>
+                       <li>{this.props.quiz_question.answer_options[0]}</li>
+                    </ul>
+                </section>
+            </main>
+        )
+    }
+}
+export default QuizQuestion
