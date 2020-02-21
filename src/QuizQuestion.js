fcd8de562b27ebd0d5c2b6d14dd0df75a6ae8123
Thomas Wirz <t.wirz@adeon.ch>
2020-02-21 21:28:51 +0100

    complete modul 5

--- src/QuizQuestion.js
+++ src/QuizQuestion.js
@@ -4,6 +4,11 @@ import QuizQuestionButton from './QuizQuestionButton.js'
 class QuizQuestion extends Component{
 
     render(){
+        const items = this.props.quiz_question.answer_options.map((answer_option, index) => 
+        <li key={index}>
+            <QuizQuestionButton key={index} clickHandler={this.handleClick.bind(this)} button_text=
+        {answer_option}/>
+        </li>);
         return (
             <main>
                 <section>
@@ -13,11 +18,20 @@ class QuizQuestion extends Component{
                 </section>
                 <section className="buttons">
                     <ul>
-                       <QuizQuestionButton button_text={this.props.quiz_question.answer_options[0]}/>
+                        {items}
+                       
                     </ul>
                 </section>
             </main>
         )
+    }
+    handleClick(buttonText){
+        if(this.props.quiz_question.answer === buttonText){
+            this.props.showNextQuestionHandler()
+        }
+        else{
+        alert("wrong answer");
+        }
     }
 }
 export default QuizQuestion
