fb0200d8974c75e5160843fdf3e9be4f2f8783c4
Thomas Wirz <t.wirz@adeon.ch>
2020-02-21 22:04:08 +0100

    complete module 7

--- src/Quiz.js
+++ src/Quiz.js
@@ -13,14 +13,17 @@ class Quiz extends Component{
     const isQuizEnd = ((this.state.quiz_position-1) === quizData.quiz_questions.length);
      return (
          <div>
-             {isQuizEnd ? <QuizEnd/> :
+             {isQuizEnd ? <QuizEnd resetClickHandler={this.handleResetClick.bind(this)}/> :
              <QuizQuestion showNextQuestionHandler={this.showNextQuestion.bind(this)} quiz_question={quizData.quiz_questions[this.state.quiz_position -1 ]}/>
             }
         </div>
      )
  }
  showNextQuestion(){
-    this.setState({quiz_position: this.state.quiz_position + 1})
+    this.setState({quiz_position: this.state.quiz_position + 1});
+ }
+ handleResetClick(){
+    this.setState({quiz_position: 1});
  }
 }
 export default Quiz
